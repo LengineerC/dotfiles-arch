f30d4d7a2b29cdba1eee2f87f3210bb7143b135e
LengineerC <lengineerc@outlook.com>
2026-08-19 19:24:24 +0800

    2026-08-19 19:24:24

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,3 +3,11 @@
 -- Add any additional options here
 
 vim.opt.spelllang = {}
+
+if vim.g.neovide then
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_opacity = 0.93
+  vim.g.neovide_normal_opacity = 0.93
+  vim.g.neovide_position_animation_length = 0.2
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
